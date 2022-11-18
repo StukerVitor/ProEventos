9018c5d92574767753f7d34cc109b68a0ac150a0
Vitor Stuker <Vitor.Stuker@Dellteam.com>
2022-11-18 10:19:54 -0300

    grid view

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados : any = [];
+
+  larguraImg : number = 150;
+  margemImg : number = 2;
+  exibirImagem : boolean = true;
+  private _filtroLista : string = '';
+
+  public get filtroLista() : string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(filtro:string){
+    this._filtroLista = filtro;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos; //verifica se tem valor no _filtroLista
+  }
+
+  filtrarEventos(filtro:string) : any {
+    filtro = filtro.toLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtro) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtro) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,12 +39,18 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImagem() {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos() : void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
-
   }
 }
 
